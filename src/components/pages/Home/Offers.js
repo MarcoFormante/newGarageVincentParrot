8091ee54c0dbefedf5ba2bbd15c183d93e736136
marciuto <formante.marco@gmail.com>
2023-05-25 19:46:13 +0200

    car-cards offers section: add buttons for scrolling carousel

--- src/components/pages/Home/Offers.js
+++ src/components/pages/Home/Offers.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React,{ useState,useEffect,useRef } from 'react'
 import CarCard from '../../CarCard/CarCard'
 import ButtonCta from '../../Buttons/ButtonCta'
 
+
 const Offers = () => {
+    const [arrowTarget, setArrowTarget] = useState();
+    const [carouselX, setCarouselX] = useState(0);
+    const carousel = useRef();
+    const cardPadding = 20;
+
+    //carousel scroll event
+    const handleScrollCarousel = (dir) => {
+            carousel.current.scrollBy({
+                top: 0,
+                left: dir + window.innerWidth - cardPadding,
+                behavior: "smooth"
+            })
+            setTimeout(() => {
+                setCarouselX(carousel.current.scrollLeft)
+                setArrowTarget("")
+            },500) 
+        }
+
+    useEffect(() => {
+        
+        switch (arrowTarget) {
+            
+            case "left":
+                handleScrollCarousel("-")
+                break;
+            case "right":
+                handleScrollCarousel("+")
+                break;
+        
+            default:
+                break;
+        }
+    },[arrowTarget])
+
     /* provisoire!! fetch data Count(card_offers)*/
     const offerCards = [
         {
@@ -38,6 +73,88 @@ const Offers = () => {
             offer: 700,
             price: 6800
         }
+
+        ,{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        }
+
+        ,{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        }
+
+        ,{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        }
+
+        ,{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        }
+
+        ,{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        },{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        },{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        },{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        },{
+            id: 3,
+            imgPath:"/images/bkhome-mb.jpg",
+            model: "Mazda steng 98k",
+            km: 80000,
+            year: 2001,
+            offer: 700,
+            price: 6800
+        }
     ]
 
     if (!offerCards.length > 0) {
@@ -46,10 +163,16 @@ const Offers = () => {
 
     return (
         <>
-        <h3 className={'section_title section_title_offres'}>Nos offres du mois</h3> 
+            <h3 className={'section_title section_title_offres'}>Nos offres du mois</h3> 
+            <div className={"arrows_container"}>
+                <button className={'arrow arrow_left_car_carousel'} onClick={() => setArrowTarget("left")}>
+                    <div className={'arrow_left'} ></div>
+            </button >
+                <button className={'arrow  arrow_right_car_carousel'}  onClick={()=> setArrowTarget("right")}><div className={'arrow_right'}></div></button>
+            </div>
         <div className={"section_page section_page--grey"}>
       
-            <div className={'carCards_container'}>
+            <div className={'carCards_container'} ref={carousel}>
             <div className={'page_section page_section_offers card_carousel_flex'}>
                  {offerCards.map((car, index) => <CarCard key={index + car.id } {...offerCards[index]} />)}
             </div>
