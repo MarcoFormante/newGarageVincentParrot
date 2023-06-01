41c14d26aabc851415dcc731df580868b3bc79b8
marciuto <formante.marco@gmail.com>
2023-06-01 12:33:46 +0200

    prepare parc auto page

--- src/components/pages/ParcAuto/ParcAuto.js
+++ src/components/pages/ParcAuto/ParcAuto.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import PageTitle from '../../PageTitle/PageTitle'
+import CarCard from '../../CarCard/CarCard'
 
 const ParcAuto = () => {
+  const [cars, setCars] = useState([])
+  
+  useEffect(() => {
+  
+  }, [])
+  
   return (
       <div>
           <PageTitle pageTitle={"Notre Parc automobile"}/>
-      parcAuto
+
     </div>
   )
 }
