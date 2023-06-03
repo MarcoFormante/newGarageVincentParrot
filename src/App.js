977aa13e8c5f74b630de0a94dba534d47a45e650
marciuto <formante.marco@gmail.com>
2023-06-03 23:49:38 +0200

    reload active Navlink losted deleting branch / add contact route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React,{useState} from 'react'
 import Header from './components/header/Header';
 import Home from './components/pages/Home/Home';
-import PageTitle from './components/PageTitle/PageTitle';
 import TimeOpeningBlock from './components/pages/Home/TimeOpeningBlock';
 import Footer from './components/Footer/Footer';
 import ParcAuto from './components/pages/ParcAuto/ParcAuto';
+import CarDetails from './components/pages/ParcAuto/CarDetails';  
 import { Routes, Route } from 'react-router-dom';
-import CarDetails from './components/pages/ParcAuto/CarDetails';
+
 
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
         <Route exact path='/' element={<Home pageTitle={(title) => setPageTitle(title)} />} />
         <Route path='/parc-auto' element={<ParcAuto />} />
         <Route path='/parc-auto/details/:id' element={<CarDetails />} />
+        <Route path="/contact" />
         <Route path='*' element={"NOT FOUND 404"} />
       </Routes>
       <TimeOpeningBlock />
