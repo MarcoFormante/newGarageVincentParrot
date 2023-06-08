f192d325fee9dd1b23cc3c16c0222357092841a4
marciuto <formante.marco@gmail.com>
2023-06-09 01:29:10 +0200

    new-car page: create dinamic equipement input

---
+++ src/components/pages/NewCar/DetailsInputs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import FormElement from '../../FormElement/FormElement'
+
+const DetailsInputs = () => {
+  return (
+    <>
+        <FormElement label={{ for: "make", text: "Marque" }} input={{ type: "text",name:"make",id:"make",}} required={true} />
+        <FormElement label={{ for: "model", text: "Modèl" }} input={{ type: "text",name:"model",id:"model",}} required={true} />
+        <FormElement label={{ for: "price", text: "Prix" }} input={{ type: "number",name:"price",id:"price",}} required={true} />
+        <FormElement label={{ for: "year", text: "Année" }} input={{ type: "number",name:"year",id:"year",}} required={true} />
+        <FormElement label={{ for: "km", text: "Kilometrage" }} input={{ type: "number",name:"km",id:"km",}} required={true} />
+        <FormElement label={{ for: "seats", text: "Sièges" }} input={{ type: "number",name:"seats",id:"seats",}} required={true} />
+        <FormElement label={{ for: "color", text: "Couleur" }} input={{ type: "text",name:"color",id:"color",}} required={true} />
+        <FormElement label={{ for: "dni", text: "Puissance DIN" }} input={{ type: "number",name:"dni",id:"dni",}} required={true} />
+        <FormElement label={{ for: "fiscalPower", text: "Puissance fiscale" }} input={{ type: "number",name:"fiscalPower",id:"fiscalPower",}} required={true} />
+        <FormElement label={{ for: "gearbox", text: "Boite de vitesse" }} select={{ options: ["Manuelle","Automatique"] , name:"gearbox",id:"gearbox"}} required={true} />
+        <FormElement label={{ for: "fuel", text: "Energie" }} select={{ options: ["Essence","Gazole","Électrique","GPL"] , name:"fuel",id:"fuel"}} required={true} />
+        <FormElement label={{ for: "doors", text: "Portières" }} input={{ type: "number",name:"doors",id:"doors",}} required={true} />
+        <FormElement label={{ for: "VO", text: "Numero VO" }} input={{ type: "number",name:"VO",id:"VO",}} required={true} />
+        <FormElement label={{ for: "offer", text: "Offre?" }} input={{ type: "number",name:"offer",id:"offer"}} required={true} />
+    </>
+  )
+}
+
+export default DetailsInputs
--- src/components/pages/NewCar/NewCarPage.js
+++ src/components/pages/NewCar/NewCarPage.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
 import PageTitle from '../../PageTitle/PageTitle'
 import FormElement from '../../FormElement/FormElement'
+import DetailsInputs from './DetailsInputs'
+
+
+
 
 const NewCarPage = () => {
+  const equipListContainer = useRef()
+  const [equipList,setEquipeList]=useState([])
+  
+  function createEquipementInput() {
+    setEquipeList(
+      prev => [...prev,
+        <FormElement label={{ for: "fuel", text: "Energie" }}
+          select={{
+            options: ["Essence", "Gazole", "Électrique", "GPL"],
+            name: "fuel", id: "fuel"
+          }} required={true}
+        />
+  ])
+  }
+
+  function deleteEquipment(index) {
+    let equipements = equipList.filter(equip => equip !== equipList[index])
+    setEquipeList([...equipements])
+  }
+
   return (
     <div>
         <PageTitle pageTitle={"Nouveau véhicule"} />
@@ -20,28 +45,19 @@ const NewCarPage = () => {
                     <div className='new_car_details container--pad-top'>
                       <span className='new_car_details_title'>Détails du vehicule</span>
                       <div className='row_inputs_container' >
-                        <FormElement label={{ for: "make", text: "Marque" }} input={{ type: "text",name:"make",id:"make",}} required={true} />
-                        <FormElement label={{ for: "model", text: "Modèl" }} input={{ type: "text",name:"model",id:"model",}} required={true} />
-                        <FormElement label={{ for: "price", text: "Prix" }} input={{ type: "number",name:"price",id:"price",}} required={true} />
-                        <FormElement label={{ for: "year", text: "Année" }} input={{ type: "number",name:"year",id:"year",}} required={true} />
-                        <FormElement label={{ for: "km", text: "Kilometrage" }} input={{ type: "number",name:"km",id:"km",}} required={true} />
-                        <FormElement label={{ for: "seats", text: "Sièges" }} input={{ type: "number",name:"seats",id:"seats",}} required={true} />
-                        <FormElement label={{ for: "color", text: "Couleur" }} input={{ type: "text",name:"color",id:"color",}} required={true} />
-                        <FormElement label={{ for: "dni", text: "Puissance DIN" }} input={{ type: "number",name:"dni",id:"dni",}} required={true} />
-                        <FormElement label={{ for: "fiscalPower", text: "Puissance fiscale" }} input={{ type: "number",name:"fiscalPower",id:"fiscalPower",}} required={true} />
-                        <FormElement label={{ for: "gearbox", text: "Boite de vitesse" }} select={{ options: ["Manuelle","Automatique"] , name:"gearbox",id:"gearbox"}} required={true} />
-                        <FormElement label={{ for: "fuel", text: "Energie" }} select={{ options: ["Essence","Gazole","Électrique","GPL"] , name:"fuel",id:"fuel"}} required={true} />
-                        <FormElement label={{ for: "doors", text: "Portières" }} input={{ type: "number",name:"doors",id:"doors",}} required={true} />
-                        <FormElement label={{ for: "VO", text: "Numero VO" }} input={{ type: "number",name:"VO",id:"VO",}} required={true} />
-                        <FormElement label={{ for: "offer", text: "Offre?" }} input={{ type: "number",name:"offer",id:"offer"}} required={true} />
+                        <DetailsInputs/>
                       </div>
                     </div>
 
                     <div className='new_car_details container--pad-top'>
-                      <span className='new_car_details_title new_car_details_title--black'>Equipements</span>
-                      <div className='row_inputs_container' >
-                        
-                      </div>
+                    <span className='new_car_details_title new_car_details_title--black'>Equipements</span>
+                    <span  className={"new_car_add-equipement"} onClick={createEquipementInput}>+</span>
+                        <ul ref={equipListContainer}>
+                          {equipList && equipList.map((equip, index) =>
+                            <li key={"equip_list" + index + "i" + Math.floor(Math.random() * 200)}>{equip} <span onClick={()=>deleteEquipment(index)}>Supprime</span></li>  
+                           
+                           )}
+                        </ul>
                     </div>
 
                   </div>
